6dd9b84f99b12c77f4839be7dc1f7ba672bea6c0
lbiester <laurajpb@gmail.com>
2014-10-19 16:27:19 +0800

    Changes to data model
    One name field, Room Number must be String

--- Django/students/models.py
+++ Django/students/models.py
@@ -3,12 +3,11 @@ from django.db import models
 # running python manage.py migrate should create this table when the database info is filled in
 # to see the actual SQL, run python manage.py sqlmigrate
 class Student(models.Model):
-    studentFirstName = models.TextField()
-	studentLastName = models.TextField()
+    studentName = models.TextField()
 	dormName = models.TextField()
 	dormFloor = models.IntegerField()
-	dormRoomNumber = models.IntegerField()
-	# this field will ideally not include a @carleton.com so that it can be used ot link to photos
+	dormRoomNumber = models.CharField(max_length=6)
+	# this field will ideally not include a @carleton.com so that it can be used to link to photos
 	studentEmail = models.TextField()
 	proxy = models.TextField()
 	# the same should happen here for consistency

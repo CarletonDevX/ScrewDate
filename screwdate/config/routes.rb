c3e9e8a3feb5cd3671080ec8877108bbddb8b9c5
lbiester <laurajpb@gmail.com>
2014-12-21 22:42:33 -0500

    (currently static) search page can now be accessed through rails

--- screwdate/config/routes.rb
+++ screwdate/config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
    root 'home#index'
 
+   get 'search' => 'search#search'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 

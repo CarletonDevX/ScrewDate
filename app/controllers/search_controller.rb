b0996332b1b85c4b97475aabe45cc7a7e28d4e25
lbiester <laurajpb@gmail.com>
2014-12-22 22:36:15 -0500

    Dropdown populated from database. Need to work on majors (clearly)

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,2 +1,12 @@
 class SearchController < ApplicationController
+  def index
+    # this needs serious reworking to deal with double majors
+    # or we can store majors as a many to one relationship with students
+    # need to think it over more
+    @majors = Student.uniq.pluck(:major)
+    @dorms = Student.uniq.pluck(:dormName)
+    @years = Student.uniq.pluck(:classYear)
+    @floors = Student.uniq.pluck(:dormFloor)
+
+  end
 end

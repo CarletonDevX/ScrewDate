297989e7c88aad82a6c27b1682ea722686b7c79f
lbiester <laurajpb@gmail.com>
2014-12-23 00:07:28 -0500

    Fixes for new column names, added ordering and filtering on dropdown data

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,10 +3,11 @@ class SearchController < ApplicationController
     # this needs serious reworking to deal with double majors
     # or we can store majors as a many to one relationship with students
     # need to think it over more
-    @majors = Student.uniq.pluck(:major)
-    @dorms = Student.uniq.pluck(:dormName)
-    @years = Student.uniq.pluck(:classYear)
-    @floors = Student.uniq.pluck(:dormFloor)
+    @majors = Student.order(:major).uniq.pluck(:major)
+    @dorms = Student.order(:dorm_name).uniq.pluck(:dorm_name)
+    @years = Student.order(:class_year).uniq.pluck(:class_year)
+    @floors = Student.uniq.order(:dorm_floor)
+      .where('dorm_floor != ?', -1).pluck(:dorm_floor)
 
   end
 end

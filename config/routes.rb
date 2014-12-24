ecd7a91c2edc45ab34e61267d2574271ac3ec08d
chetaldrich <chetaldrich@gmail.com>
2014-12-24 15:49:27 -0600

    Structure beginning new user signup page, links

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
+  root 'home#index'
+  get 'signup' => 'users#new'
   get 'home/index'
-
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root 'home#index'
+
 
    resources :search
 

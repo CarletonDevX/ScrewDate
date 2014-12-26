f62d5d82ae6618a378f198fb08e0e585fde5650c
chetaldrich <chetaldrich@gmail.com>
2014-12-26 15:18:18 -0600

    add tests and validation for presence of email and name in model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -9,5 +9,15 @@ class UserTest < ActiveSupport::TestCase
   test "should return as valid" do
     assert @user.valid?
   end
+
+  test "check for name" do
+    @user.name = "      "
+    assert_not @user.valid?
+  end
+
+  test "check for email" do
+    @user.email = "       "
+    assert_not @user.valid?
+  end
   
 end

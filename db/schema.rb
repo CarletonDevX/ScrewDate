93f3fe01c06c9bad3f4e3354763c5216ab11566d
chetaldrich <chetaldrich@gmail.com>
2014-12-25 19:06:58 -0600

    Create users controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141225230658) do
+ActiveRecord::Schema.define(version: 20141226005820) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,13 @@ ActiveRecord::Schema.define(version: 20141225230658) do
     t.string  "major"
     t.integer "proxy_id"
     t.integer "date_id"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end

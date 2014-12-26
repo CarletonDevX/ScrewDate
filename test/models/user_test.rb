0a73addf135584615cf47d58bcc19097acfe816b
chetaldrich <chetaldrich@gmail.com>
2014-12-26 14:11:40 -0600

    created test for user generation in db

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @user = User.new(name: "User", email: "user@example.com")
+  end
+
+  test "should return as valid" do
+    assert @user.valid?
+  end
+  
 end

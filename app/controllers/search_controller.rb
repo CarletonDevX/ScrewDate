342d0152eadfb83aa58f8a876fb9f5c9cb744c6e
lbiester <laurajpb@gmail.com>
2014-12-27 00:51:32 -0500

    Update controller due to DB changes

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,13 +1,9 @@
 class SearchController < ApplicationController
   def index
-    # this needs serious reworking to deal with double majors
-    # or we can store majors as a many to one relationship with students
-    # need to think it over more
-    @majors = Student.order(:major).uniq.pluck(:major)
-    @dorms = Student.order(:dorm_name).uniq.pluck(:dorm_name)
+    @majors = Major.order(:major_name).pluck(:major_name)
+    @dorms = Dorm.order(:dorm_name).pluck(:dorm_name)
     @years = Student.order(:class_year).uniq.pluck(:class_year)
     @floors = Student.uniq.order(:dorm_floor)
       .where('dorm_floor != ?', -1).pluck(:dorm_floor)
-
   end
 end

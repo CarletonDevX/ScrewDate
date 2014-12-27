b67b9bb85493c8b5de2ee9aa2729540ced160cdf
lbiester <laurajpb@gmail.com>
2014-12-27 00:04:45 -0500

    Updates to models with new majors table

--- app/models/Student.rb
+++ app/models/Student.rb
@@ -3,4 +3,5 @@ class Student < ActiveRecord::Base
   has_many :proxy_for, class_name: "Student", foreign_key: "proxy_id"
   belongs_to :date_requestor, class_name: "Student", foreign_key: "date_id"
   has_many :date_requested, class_name: "Student", foreign_key: "date_id"
+  has_and_belongs_to_many :majors
 end

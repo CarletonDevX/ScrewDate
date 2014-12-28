fbe2b2ce1eacfd560e9f69a605159a3657fe3490
lbiester <laurajpb@gmail.com>
2014-12-28 15:18:11 -0500

    Options for dorm and major now have ids

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,7 @@
 class SearchController < ApplicationController
   def index
-    @majors = Major.order(:major_name).pluck(:major_name)
-    @dorms = Dorm.order(:dorm_name).pluck(:dorm_name)
+    @majors = Major.order(:major_name)
+    @dorms = Dorm.order(:dorm_name)
     @years = Student.order(:class_year).uniq.pluck(:class_year)
     @floors = Student.uniq.order(:dorm_floor)
       .where('dorm_floor != ?', -1).pluck(:dorm_floor)

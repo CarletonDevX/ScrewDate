7af140230da4c3832b603b6c071144c5d108527f
chetaldrich <chetaldrich@gmail.com>
2014-12-30 15:02:04 -0600

    Enforce user email uniqueness in db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141226195805) do
+ActiveRecord::Schema.define(version: 20141230200824) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,5 +52,7 @@ ActiveRecord::Schema.define(version: 20141226195805) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
 end

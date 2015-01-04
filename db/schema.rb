adcc6c1cb95be487ea59eae8c12d0f5fbe8d77a4
chetaldrich <chetaldrich@gmail.com>
2015-01-04 17:48:28 -0600

    migration, add bcrypt to gems

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141230200824) do
+ActiveRecord::Schema.define(version: 20150104234248) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 20141230200824) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree

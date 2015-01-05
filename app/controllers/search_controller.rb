6ce0b982a7b2eb89f4ebf4e203da2f45fd4e5cb5
lbiester <laurajpb@gmail.com>
2015-01-04 20:36:26 -0600

    Check params in search controller and on page

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,8 @@
 class SearchController < ApplicationController
   def index
+    if request.query_parameters.length != 0
+      @studentsResults = Student.all
+    end
     @majors = Major.order(:major_name)
     @dorms = Dorm.order(:dorm_name)
     @years = Student.order(:class_year)

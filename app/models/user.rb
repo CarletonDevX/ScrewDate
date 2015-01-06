e2605f9b9766f8ab723d6f5a2cbf6ce486443869
chetaldrich <chetaldrich@gmail.com>
2015-01-05 21:51:56 -0600

    getting unit tests for user class to work for the moment

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,5 @@ class User < ActiveRecord::Base
   validates :email, presence: true, length: { maximum: 50 }, format: {with: VALID_EMAIL_REGEX },
              uniqueness: { case_sensitive: false }
   
-  has_secure_password
+  has_secure_password  
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,8 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
 
   def setup
-    @user = User.new(name: "User", email: "user@example.com")
+    @user = User.new(name: "User", email: "user@example.com", 
+                     password: "password", password_confirmation: "password")
   end
 
   test "should return as valid" do

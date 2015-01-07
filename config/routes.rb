953e741dd9429716061791a22579dc72c5cf0c4f
chetaldrich <chetaldrich@gmail.com>
2015-01-06 22:03:01 -0600

    Users created in console show up in view

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,8 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
 
-
-   resources :search
+  resources :users  
+  resources :search
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'

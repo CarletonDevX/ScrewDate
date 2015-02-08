c16781a81c5c49a039be6575ecac06fb0122349b
chetaldrich <chetaldrich@gmail.com>
2015-02-07 21:25:54 -0600

    Users login functions
    Currently, some bugs with how flash messages work, which seems to have
    been a rather constant issue since I started using them.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,10 +5,11 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-
+      log_in user
+      redirect_to user
     else
       flash.now[:danger] = 'Invalid email/password combination'
-      render 'new'
+      render 'sessions/new'
     end
   end
 

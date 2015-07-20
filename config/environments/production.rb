c16ea54c754414d3737f2625a1cd3a2c7c8ecebe
lbiester <laurajpb@gmail.com>
2015-07-19 23:08:41 -0700

    scss -> sass

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,4 +75,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # use sass syntax
+  config.sass.preferred_syntax = :sass
 end
